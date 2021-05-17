b1c95c07e193c97d843fd222459964929356c128
tsamourid <thanasistsam@yahoo.com>
2021-05-17 13:22:25 +0300

    Better plot output

--- Differential_Equations_Problem_Solving.py
+++ Differential_Equations_Problem_Solving.py
@@ -5,6 +5,7 @@ import matplotlib
 
 matplotlib.rcParams['mathtext.fontset'] = 'cm'
 matplotlib.rcParams['font.family'] = 'STIXGeneral'
+legend_properties = {'weight':'bold'}
 
 F = lambda t, x: np.array([1.1 * x[0] - 0.4*x[0]*x[1], 0.4*x[0]*x[1] - 0.1*x[1]])
 x0 = np.array([ 20 , 1 ])
@@ -16,16 +17,16 @@ t, x = euler(F, x0, t0, tmax, dt)
 plt.figure()
 plt.plot(t, x[:, 0], "b-", t, x[:, 1], "r--")
 plt.grid()
-plt.title("Time Evolution of Foxes Population and Rabbits Population")
-plt.legend(["Rabbits Population", "Foxes Population"])
-plt.xlabel("Time")
-plt.ylabel("No. of Rabbits and No. of Foxes")
+plt.title("Time Evolution of Foxes Population and Rabbits Population", fontsize = 14 , fontweight="bold")
+plt.legend(["Rabbits Population", "Foxes Population"], prop = legend_properties)
+plt.xlabel("Time", fontweight="bold")
+plt.ylabel("No. of Rabbits and No. of Foxes", fontweight="bold")
 
 plt.figure()
 plt.plot(x[:, 0], x[:, 1], "k--")
 plt.grid()
-plt.title("Phase Plot")
-plt.xlabel("Rabbits Population")
-plt.ylabel("Foxes Population")
+plt.title("Phase Plot", fontsize = 14 , fontweight="bold")
+plt.xlabel("Rabbits Population", fontweight="bold")
+plt.ylabel("Foxes Population", fontweight="bold")
 
 plt.show()
